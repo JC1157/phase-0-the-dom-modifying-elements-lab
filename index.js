f4136259a043a9ea51180ba971abb6d98852d3b6
Juan Baez <juan.baez2093@gmail.com>
2022-10-12 16:55:29 -0400

    commit tree a9c9613bcc0fd946f7cfacd6a60e0c96c5cb56a1

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+document.getElementById("main").remove();
+
+const newHeader = document.createElement("h1");
+document.body.append(newHeader); 
+
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Juan is the champion';
+
+
